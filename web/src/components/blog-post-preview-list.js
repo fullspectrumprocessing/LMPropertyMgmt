35ea8fa08d507afc2320ca4a6e5aadaeed712d60
Kathleenmont <kathleen.m.montesano@gmail.com>
2020-02-18 16:41:58 -0500

    header styles and limited landing page posts

--- web/src/components/blog-post-preview-list.js
+++ web/src/components/blog-post-preview-list.js
@@ -1,20 +1,28 @@
-import {Link} from 'gatsby'
-import React from 'react'
-import BlogPostPreview from './blog-post-preview'
+import { Link } from "gatsby";
+import React from "react";
+import BlogPostPreview from "./blog-post-preview";
 
-import styles from './blog-post-preview-list.module.css'
+import styles from "./blog-post-preview-list.module.css";
 
-function BlogPostPreviewGrid (props) {
+function BlogPostPreviewGrid(props) {
   return (
     <div className={styles.root}>
       {props.title && <h2 className={styles.headline}>{props.title}</h2>}
-      <ul className={styles.grid}>
+      {/* <ul className={styles.grid}>
         {props.nodes &&
           props.nodes.map(node => (
+          
             <li key={node.id}>
               <BlogPostPreview {...node} isInList />
+              {console.log(node)}
             </li>
           ))}
+      </ul> */}
+      <ul className={styles.grid}>
+        <li key={props.nodes[0].id}>
+          <BlogPostPreview {...props.nodes[0]} isInList />
+          {console.log(props.nodes[0])}
+        </li>
       </ul>
       {props.browseMoreHref && (
         <div className={styles.browseMoreNav}>
@@ -22,13 +30,13 @@ function BlogPostPreviewGrid (props) {
         </div>
       )}
     </div>
-  )
+  );
 }
 
 BlogPostPreviewGrid.defaultProps = {
-  title: '',
+  title: "",
   nodes: [],
-  browseMoreHref: ''
-}
+  browseMoreHref: ""
+};
 
-export default BlogPostPreviewGrid
+export default BlogPostPreviewGrid;
