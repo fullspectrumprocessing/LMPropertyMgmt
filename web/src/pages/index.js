1977b368110aded379a43fbed7a9b89630a5a268
Kathleenmont <kathleen.m.montesano@gmail.com>
2020-03-03 17:30:41 -0500

    test

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -105,6 +105,7 @@ const IndexPage = props => {
       <HomeHero />
       <HomeTextSec />
       <Container>
+        <h1>Test</h1>
         {/* <h1 >Here is the Title</h1> */}
         {postNodes && (
           <BlogPostPreviewList
