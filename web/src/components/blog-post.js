e5337398a8def7969547743d8c15eb708d93c14c
Kathleenmont <kathleen.m.montesano@gmail.com>
2020-03-06 17:52:28 -0500

    image slider

--- web/src/components/blog-post.js
+++ web/src/components/blog-post.js
@@ -5,15 +5,19 @@ import {imageUrlFor} from '../lib/image-url'
 import PortableText from './portableText'
 import Container from './container'
 import AuthorList from './author-list'
-
+import Slider from 'infinite-react-carousel';
 import styles from './blog-post.module.css'
 
 function BlogPost (props) {
   console.log(props)
   // const {_rawBody, authors, categories, title, mainImage, publishedAt} = props
-  const {authors, categories, title, mainImage, publishedAt, _rawDescription, price, bedrooms, bathrooms, neighborhood, squareFeet, address, dateAvailable} = props
+  const {authors, categories, title, mainImage, postImage1, postImage2, postImage3, publishedAt, _rawDescription, price, bedrooms, bathrooms, neighborhood, squareFeet, address, dateAvailable} = props
   return (
+    
     <article className={styles.root}>
+      <div className={styles.imageWrap}>
+<div className={styles.sliderWrapper}>
+<Slider dots >
       {mainImage && mainImage.asset && (
         <div className={styles.mainImage}>
           <img
@@ -27,6 +31,50 @@ function BlogPost (props) {
           />
         </div>
       )}
+     
+      {postImage1 && postImage1.asset && (
+        <div className={styles.mainImage}>
+      <img
+            src={imageUrlFor(buildImageObj(postImage1))
+              .width(1200)
+              .height(Math.floor((9 / 16) * 1200))
+              .fit('crop')
+              .auto('format')
+              .url()}
+            alt={postImage1.alt}
+          />
+     </div>
+      )}
+       {postImage1 && postImage2.asset && (
+        <div className={styles.mainImage}>
+      <img
+            src={imageUrlFor(buildImageObj(postImage2))
+              .width(1200)
+              .height(Math.floor((9 / 16) * 1200))
+              .fit('crop')
+              .auto('format')
+              .url()}
+            alt={postImage2.alt}
+          />
+     </div>
+      )}
+       {postImage1 && postImage3.asset && (
+        <div className={styles.mainImage}>
+      <img
+            src={imageUrlFor(buildImageObj(postImage3))
+              .width(1200)
+              .height(Math.floor((9 / 16) * 1200))
+              .fit('crop')
+              .auto('format')
+              .url()}
+            alt={postImage3.alt}
+          />
+     </div>
+      )}
+  
+      </Slider>
+      </div>
+      </div>
       <Container>
         <div className={styles.grid}>
           <div className={styles.mainContent}>
@@ -56,7 +104,15 @@ function BlogPost (props) {
           </aside>
         </div>
       </Container>
-    </article>
+
+   <style jsx>{`
+   .carousel-dots li button:before {
+    color: white !important;
+
+  }
+  
+   `}</style>
+       </article>
   )
 }
 
