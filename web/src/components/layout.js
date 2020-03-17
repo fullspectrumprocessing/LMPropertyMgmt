3e6cb8590c8323b99d4f63c187b528c54fecfb9d
Kathleen Montesano <kathleen.m.montesano@gmail.com>
2020-03-17 17:19:05 -0400

    lower footer styles

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -60,13 +60,14 @@ const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => {
                     </div>
                     <div></div>
                 </div>
+                </div>
                 <div className={styles.siteInfo}>
                     &copy; {new Date().getFullYear()}, Built with{' '}
                     <a href="https://www.sanity.io">Sanity</a> &amp;
                     {` `}
                     <a href="https://www.gatsbyjs.org">Gatsby</a>
                 </div>
-            </div>
+          
         </footer>
     </>
 )
